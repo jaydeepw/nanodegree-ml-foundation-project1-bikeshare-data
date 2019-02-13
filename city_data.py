7567a145052f6ac2f3b8d088c43e84f501fa50f6
jaydeepw <jaydeep.w@gmail.com>
2019-02-13 13:21:58 +0530

    Add an outermost loop to let the user continue with the analysis even when first round of analysis is complete.

--- city_data.py
+++ city_data.py
@@ -97,96 +97,116 @@ def trip_duration(df):
     averageDuration = totalDuration / numRows
     return totalDuration, averageDuration
 
+def show_head_data_conditionally(response, df):
+    if response == "no":
+        return
+    
+    print(df.head())
+
 # df = load_data('washington', 'all', 'Friday')
 # trip_duration(df)
 
 while True:
-    print("Which city you want to get data for?")
-    cityCodes = CITY_CODES_NAMES.keys()
-    cityNames = CITY_CODES_NAMES.values()
-    for item in range(len(CITY_CODES_NAMES)):
-        print("Enter {} for {}".format(cityCodes[item], cityNames[item]))
-    
-    cityCode = raw_input()
 
-    if (cityCode in CITY_CODES_NAMES.keys()):
-        print("Alright, you want to know about {}".format(CITY_CODES_NAMES[cityCode]))
-        break
-    else:
-        print("====> Invalid input for city name. Pleas try again.")
+    while True:
+        print("Which city you want to get data for?")
+        cityCodes = CITY_CODES_NAMES.keys()
+        cityNames = CITY_CODES_NAMES.values()
+        for item in range(len(CITY_CODES_NAMES)):
+            print("Enter {} for {}".format(cityCodes[item], cityNames[item]))
+        
+        cityCode = raw_input()
 
-while True:
-    print("Which month you want to get data for?")
-    print("Please enter index number.")
-    monthIndex = raw_input("    1. Jan \n\
+        if (cityCode in CITY_CODES_NAMES.keys()):
+            print("Alright, you want to know about {}".format(CITY_CODES_NAMES[cityCode]))
+            break
+        else:
+            print("====> Invalid input for city name. Pleas try again")
+
+    while True:
+        print("Which month you want to get data for?")
+        print("Please enter index number.")
+        monthIndex = raw_input("\
+    1. Jan \n\
     2. Feb \n\
     3. March \n\
     4. April \n\
     5. May \n\
     6. June \n")
 
-    """
-    print("monthIndex {}".format(monthIndex))
-    print("in range {}".format(monthIndex in range(1, 7)))
-    print(monthIndex >= 1)
-    print(monthIndex < 7)
-    """
+        """
+        print("monthIndex {}".format(monthIndex))
+        print("in range {}".format(monthIndex in range(1, 7)))
+        print(monthIndex >= 1)
+        print(monthIndex < 7)
+        """
 
-    #todo: This is currently not working. fix it.
-    if monthIndex in range(1, 7):
-        print("Month is in range")
+        #todo: This is currently not working. fix it.
+        if monthIndex in range(1, 7):
+            print("Month is in range")
+            break
+        else:
+            print("====> Invalid input for month")
+            #todo remove this break once above if block start working
+            break
+
+        # todo: check invalid input
+        print("My month is {} {}".format(monthIndex, cityName))
+
+    while True:
+        print("Which day you want to get data for?")
+        print("Please enter index number.")
+        dayIndex = raw_input("    0. Mon \n\
+        1. Tuesday \n\
+        2. Wednesday \n\
+        3. Thursday \n\
+        4. Friday \n\
+        5. Saturday \n\
+        6. Sunday\n")
+
+        #todo: This is currently not working. fix it.
+        if dayIndex in range(0, 7):
+            print("Day is in range")
+            break
+        else:
+            print("====> Invalid input for day")
+            #todo remove this break once above if block start working
+            break
+
+        # todo: check invalid input
+        print("My city: {} \n month {} \n dayIndex {}".format(cityCode, monthIndex, dayIndex))
+
+    # calculate the statistics
+    print("Counting stats....")
+    cityName = CITY_CODES_NAMES[cityCode].lower()
+    df = load_data(cityName, 'all', 'Thursday')
+
+    # Print the statistics
+    print("Printing analysis")
+
+    # Print percentage if male sharers
+    percentage = get_male_sharer_percentage(df)
+
+    if percentage > 0:
+        print("Approximately {} percent of the total particiapnt are Male".format(percentage))
+
+    # Print most popular hour and its count
+    popular_hour, count = popular_hour_of_the_day(df)
+
+    print("Most popular hour of the day was {}".format(popular_hour))
+    print("{} sharings happened during this hour".format(count))
+
+    # Print duration stats
+    totalDuration, averageDuration = trip_duration(df)
+    print("Riders from this city spent a total of {} seconds riding".format(totalDuration))
+    print("Average duration of the riders has been {} seconds".format(averageDuration))
+
+    show_head_data_conditionally(raw_input("See 5 lines of raw data? Enter yes/no \n"), df)
+
+    # Outermost big loop counting here
+    print("Do you want more analysis of other cities, months or days?")
+    more_analysis = raw_input("yes/no \n")
+    if more_analysis == "no":
+        # break top-most while loop
+        print("Thank you for using this tool")
         break
-    else:
-        print("====> Invalid input for month")
-        #todo remove this break once above if block start working
-        break
-
-    # todo: check invalid input
-    print("My month is {} {}".format(monthIndex, cityName))
-
-while True:
-    print("Which day you want to get data for?")
-    print("Please enter index number.")
-    dayIndex = raw_input("    0. Mon \n\
-    1. Tuesday \n\
-    2. Wednesday \n\
-    3. Thursday \n\
-    4. Friday \n\
-    5. Saturday \n\
-    6. Sunday")
-
-    #todo: This is currently not working. fix it.
-    if dayIndex in range(0, 7):
-        print("Day is in range")
-        break
-    else:
-        print("====> Invalid input for day")
-        #todo remove this break once above if block start working
-        break
-
-    # todo: check invalid input
-    print("My city: {} \n month {} \n dayIndex {}".format(cityCode, monthIndex, dayIndex))
-
-
-# Print the statistics
-cityName = CITY_CODES_NAMES[cityCode].lower()
-df = load_data(cityName, 'all', 'Thursday')
-
-print("Printing analysis")
-
-# Print percentage if male sharers
-percentage = get_male_sharer_percentage(df)
-
-if percentage > 0:
-    print("Approximately {} percent of the total particiapnt are Male".format(percentage))
-
-# Print most popular hour and its count
-popular_hour, count = popular_hour_of_the_day(df)
-
-print("Most popular hour of the day was {}".format(popular_hour))
-print("{} sharings happened during this hour".format(count))
-
-# Print duration stats
-totalDuration, averageDuration = trip_duration(df)
-print("Riders from this city spent a total of {} seconds riding".format(totalDuration))
-print("Average duration of the riders has been {} seconds".format(averageDuration))
